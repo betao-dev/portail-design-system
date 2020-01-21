ce924e40010fccd151e9a227d4d9d9b68652d233
Donghai <priceofjs@gmail.com>
2020-01-21 10:35:41 -0800

    Update Form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -30,7 +30,7 @@ export default {
         for (let i = 0; i < currentFormElements.length; i++) {
           if (currentFormElements[i].tagName !== 'BUTTON') {
             if (!currentFormElements[i].name) return
-            if (currentFormElements[i].classList.value.includes('ds-error')) return
+            if (currentFormElements[i].className.includes('ds-error')) return
             emitValue[currentFormElements[i].name] = currentFormElements[i].value
           }
         }
