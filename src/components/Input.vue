1dca95b100f0d31f6d1ab4cbea2f7a290a27168f
anton <anton.serdiukov.dev@gmail.com>
2020-02-16 01:16:37 +0200

    refact poper styles of Input,Tooltip, update doc

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -528,124 +528,19 @@ export default {
 
         &::v-deep {
           .popper {
-            background-color: @color-gray-500;
-            border-radius: 2px;
-            color: @color-white;
-            border: solid 1px @color-gray-500;
-            font-family: @font-family;
             font-size: 12px;
             line-height: 16px;
-            padding: 16px;
-            position: relative;
-            text-align: left;
-            box-shadow: none;
-            margin-top: 10px;
-            margin-bottom: 10px;
             max-width: 330px;
             min-width: 300px;
             width: auto;
             white-space: initial;
-
-            &[x-placement^="bottom"] {
-              [x-arrow] {
-                &:before {
-                  content: '';
-                  position: absolute;
-                  left: -10px;
-                  top: -5px;
-                  border-bottom: 15px solid @color-gray-500;
-                  border-right: 15px solid transparent;
-                  border-left: 15px solid transparent;
-                  z-index: 1;
-                }
-
-                &:after {
-                  content: '';
-                  position: absolute;
-                  left: -10px;
-                  top: -5px;
-                  border-bottom: 15px solid @color-gray-500;
-                  border-right: 15px solid transparent;
-                  border-left: 15px solid transparent;
-                  z-index: 2;
-                }
-              }
-            }
-
-            &[x-placement^="top"] {
-              [x-arrow] {
-                &:before {
-                  content: '';
-                  position: absolute;
-                  left: -10px;
-                  bottom: -5px;
-                  border-top: 15px solid @color-gray-500;
-                  border-right: 15px solid transparent;
-                  border-left: 15px solid transparent;
-                  z-index: 1;
-                }
-
-                &:after {
-                  content: '';
-                  position: absolute;
-                  left: -10px;
-                  bottom: -5px;
-                  border-top: 15px solid @color-gray-500;
-                  border-right: 15px solid transparent;
-                  border-left: 15px solid transparent;
-                  z-index: 2;
-                }
-              }
-            }
+            .poper();
 
             &[x-placement^="left"] {
               [x-arrow] {
                 &:before {
-                  content: '';
-                  position: absolute;
                   top: -5px;
                   right: -10px;
-                  border-left: 15px solid @color-gray-500;
-                  border-top: 15px solid transparent;
-                  border-bottom: 15px solid transparent;
-                  z-index: 1;
-                }
-
-                &:after {
-                  content: '';
-                  position: absolute;
-                  top: -10px;
-                  right: -5px;
-                  border-left: 15px solid @color-gray-500;
-                  border-top: 15px solid transparent;
-                  border-bottom: 15px solid transparent;
-                  z-index: 2;
-                }
-              }
-            }
-
-            &[x-placement^="right"] {
-              [x-arrow] {
-                &:before {
-                  content: '';
-                  position: absolute;
-                  top: -10px;
-                  left: -5px;
-                  border-right: 15px solid @color-gray-500;
-                  border-top: 15px solid transparent;
-                  border-bottom: 15px solid transparent;
-                  z-index: 1;
-                }
-
-                &:after {
-                  content: '';
-                  position: absolute;
-                  top: -10px;
-                  left: -5px;
-                  border-right: 15px solid @color-gray-500;
-                  border-top: 15px solid transparent;
-                  border-bottom: 15px solid transparent;
-                  z-index: 2;
                 }
               }
             }
--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -9,7 +9,7 @@
       <div class="popper">
         <slot name="popover"></slot>
       </div>
-  
+
       <div
         class="reference"
         slot="reference"
@@ -47,6 +47,7 @@ export default {
 
 <style lang="less" scoped>
 @import '../styles/vars';
+@import '../styles/mixins';
 
 .ds-tooltip-wrapper {
   display: inline-block;
@@ -57,124 +58,10 @@ export default {
     }
 
     .popper {
-      background-color: @color-gray-500;
-      border-radius: 2px;
-      color: @color-white;
-      border: solid 1px @color-gray-500;
-      font-family: @font-family;
       font-size: 14px;
       line-height: 18px;
-      padding: 16px;
       width: 300px;
-      position: relative;
-      text-align: left;
-      box-shadow: none;
-      margin-top: 10px;
-      margin-bottom: 10px;
-
-      &[x-placement^="bottom"] {
-        [x-arrow] {
-          &:before {
-            content: '';
-            position: absolute;
-            left: -10px;
-            top: -5px;
-            border-bottom: 15px solid @color-gray-500;
-            border-right: 15px solid transparent;
-            border-left: 15px solid transparent;
-            z-index: 1;
-          }
-
-          &:after {
-            content: '';
-            position: absolute;
-            left: -10px;
-            top: -5px;
-            border-bottom: 15px solid @color-gray-500;
-            border-right: 15px solid transparent;
-            border-left: 15px solid transparent;
-            z-index: 2;
-          }
-        }
-      }
-
-      &[x-placement^="top"] {
-        [x-arrow] {
-          &:before {
-            content: '';
-            position: absolute;
-            left: -10px;
-            bottom: -5px;
-            border-top: 15px solid @color-gray-500;
-            border-right: 15px solid transparent;
-            border-left: 15px solid transparent;
-            z-index: 1;
-          }
-
-          &:after {
-            content: '';
-            position: absolute;
-            left: -10px;
-            bottom: -5px;
-            border-top: 15px solid @color-gray-500;
-            border-right: 15px solid transparent;
-            border-left: 15px solid transparent;
-            z-index: 2;
-          }
-        }
-      }
-
-      &[x-placement^="left"] {
-        [x-arrow] {
-          &:before {
-            content: '';
-            position: absolute;
-            top: -10px;
-            right: -5px;
-            border-left: 15px solid @color-gray-500;
-            border-top: 15px solid transparent;
-            border-bottom: 15px solid transparent;
-            z-index: 1;
-          }
-
-          &:after {
-            content: '';
-            position: absolute;
-            top: -10px;
-            right: -5px;
-            border-left: 15px solid @color-gray-500;
-            border-top: 15px solid transparent;
-            border-bottom: 15px solid transparent;
-            z-index: 2;
-          }
-        }
-      }
-
-      &[x-placement^="right"] {
-        [x-arrow] {
-          &:before {
-            content: '';
-            position: absolute;
-            top: -10px;
-            left: -5px;
-            border-right: 15px solid @color-gray-500;
-            border-top: 15px solid transparent;
-            border-bottom: 15px solid transparent;
-            z-index: 1;
-          }
-
-          &:after {
-            content: '';
-            position: absolute;
-            top: -10px;
-            left: -5px;
-            border-right: 15px solid @color-gray-500;
-            border-top: 15px solid transparent;
-            border-bottom: 15px solid transparent;
-            z-index: 2;
-          }
-        }
-      }
+      .poper();
     }
   }
 
