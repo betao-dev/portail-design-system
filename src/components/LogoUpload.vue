a6ac8e0da8261e78fabcc0c51a6a60f194245cf4
anton <anton.serdiukov.dev@gmail.com>
2020-06-16 17:04:23 +0300

    update LogoUpload and prettier code

--- src/components/LogoUpload.vue
+++ src/components/LogoUpload.vue
@@ -36,8 +36,11 @@
 
     <div class="logo-footer">
       <div
-        :class="['empty-message', { 'empty-message-invalid': !isValid }]"
-        v-if="checkEmptyFile || !this.isValid"
+        :class="[
+          'empty-message',
+          { 'empty-message-invalid': !isValid || tmpInvalid }
+        ]"
+        v-if="checkEmptyFile || tmpInvalid"
       >
         Votre logo doit être au format JPG ou PNG et inférieur à 1Mo.
       </div>
@@ -103,6 +106,7 @@ export default {
             return this.fileInvalid();
           }
 
+          this.tmpInvalid = false;
           setTimeout(() => {
             this.inputValue = file.dataURL;
             this.$emit('update:isValid', true);
@@ -115,15 +119,31 @@ export default {
       },
       touched: false,
       errors: [],
-      inputValue: null
+      inputValue: null,
+      timeoutId: undefined,
+      tmpInvalid: false
     };
   },
   methods: {
     fileInvalid() {
       let message = this.dsTranslate('File invalid');
       this.$emit('invalidfile', message);
-      this.$emit('update:isValid', false);
+      if (!this.value) {
+        this.$emit('update:isValid', false);
+      } else {
+        this.setTemporaryInvalid();
+      }
       this.errors.push(message);
+    },
+    setTemporaryInvalid() {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+      }
+
+      this.tmpInvalid = true;
+      this.timeoutId = setTimeout(() => {
+        this.tmpInvalid = false;
+      }, 3000);
     },
     removeFile() {
       this.inputValue = null;
