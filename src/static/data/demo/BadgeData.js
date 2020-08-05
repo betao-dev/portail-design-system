78654d8ff702854ffeb29b8a49ad15f17ff9ca52
Donghai <priceofjs@gmail.com>
2020-08-05 05:00:12 +0200

    Update example and description of the badge component

--- src/static/data/demo/BadgeData.js
+++ src/static/data/demo/BadgeData.js
@@ -1,12 +1,56 @@
 let usage = `
-<Badge color="red">Annulée</Badge>
-<Badge color="gray">Brouillon</Badge>
-<Badge color="dark">En retard</Badge>
-<Badge color="primary">Payé</Badge>
-<Badge color="yellow">En attente</Badge>
-<Badge color="primary" icon="check_circle_outline" icon-size="20px">Done</Badge>
-<Badge color="yellow" icon="query_builder" icon-size="20px">Proccessing</Badge>
-<Badge color="red" icon="error_outline" icon-size="20px">Needs Response</Badge>
+  <Badge mark color="red" @click="toggleActive" :active="active">
+    Annulée
+  </Badge>
+
+  <Badge mark color="gray-400" @click="toggleActive" :active="active">
+    Brouillon
+  </Badge>
+
+  <Badge mark color="dark" @click="toggleActive" :active="active">
+    En retard
+  </Badge>
+
+  <Badge mark color="primary" @click="toggleActive" :active="active">
+    Payé
+  </Badge>
+
+  <Badge mark color="yellow" @click="toggleActive" :active="active">
+    En attente
+  </Badge>
+
+  <Badge
+    color="primary"
+    icon="check_circle_outline"
+    icon-size="10px"
+    icon-color="red"
+    @click="toggleActive"
+    :active="active"
+  >
+  Done
+  </Badge>
+
+  <Badge
+    color="yellow"
+    icon="query_builder"
+    icon-size="10px"
+    icon-color="red"
+    @click="toggleActive"
+    :active="active"
+  >
+    Proccessing
+  </Badge>
+
+  <Badge
+    color="red"
+    icon="error_outline"
+    icon-size="10px"
+    icon-color="red"
+    @click="toggleActive"
+    :active="active"
+  >
+    Needs Response
+  </Badge>
 `.slice(1);
 
 export default {
