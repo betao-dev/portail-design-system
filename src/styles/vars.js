3efca7bcfe51f151d6e8ab3de4b0bc9f89dadcdb
anton <anton.serdiukov.dev@gmail.com>
2020-12-24 14:07:39 +0200

    added colors and texts classes

--- src/styles/vars.js
+++ src/styles/vars.js
@@ -11,13 +11,24 @@ export const COLORS = {
   'gray-500': '#778ca2',
 
   'dark': '#2D3047',
+  'dark-100': '#252631',
+  'dark-200': '#1B1E24',
   'primary': '#1eb386',
   'white': '#ffffff',
   'blue': '#09AFFF',
   'red': '#F03F3F',
   'yellow': '#FFBA49',
   'indigo': '#745BCC',
-  'orange': '#FE7D51'
+  'orange': '#FE7D51',
+
+  'primary-hues': '#E8F7F2',
+  'red-hues': '#FEECEC',
+  'blue-hues': '#E6F7FF',
+  'yellow-hues': '#FFF6E9',
+  'indigo-hues': '#F1EFFA',
+  'orange-hues': '#FFF5F1',
+  'dark-hues': '#EAEAEC',
+  'gray-hues': '#F1F3F5'
 };
 
 export const MOBILE_WIDTH = 768;
